fbaa103b9b087621eb943605984fc0a8bfa3ddff
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-21 21:20:47 +0000

    update version
    git-svn-id: http://plugins.svn.wordpress.org/smart-archives-reloaded/trunk@156820 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smart-archives-reloaded.php
+++ smart-archives-reloaded.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Smart Archives Reloaded
-Version: 1.6.2a
+Version: 1.6.2
 Description: An elegant and easy way to present your archives.
 Author: scribu
 Author URI: http://scribu.net
